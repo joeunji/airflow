8989dc601089fbb3abe0eb7609d4caca05b88371
jonji <jon-ji@naver.com>
2024-04-12 14:53:24 +0900

    no message

--- dags/dags_python_with_task_group_240412.py
+++ dags/dags_python_with_task_group_240412.py
@@ -5,6 +5,7 @@ from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 from airflow.decorators import task_group
 from airflow.utils.task_group import TaskGroup
+from airflow.exceptions import AirflowException
 
 with DAG(
     dag_id="dags_python_with_task_group",
@@ -15,10 +16,11 @@ with DAG(
     def inner_func(**kwargs):
         msg = kwargs.get('msg') or '' 
         print(msg)
+        raise AirflowException('downstream_1 Exception!')
 
     @task_group(group_id='first_group')
     def group_1():
-        ''' task_group 데커레이터를 이용한 첫 번째 그룹입니다. '''
+        ''' task_group 데커레이터를 이용한 첫 번째 그룹입니다. '''  # docstring
 
         @task(task_id='inner_function1')
         def inner_func1(**kwargs):
@@ -27,14 +29,14 @@ with DAG(
         inner_function2 = PythonOperator(
             task_id='inner_function2',
             python_callable=inner_func,
-            op_kwargs={'msg':'첫 번째 TaskGroup내 두 번쨰 task입니다.'}
+            op_kwargs={'msg':'첫 번째 TaskGroup내 두 번쨰 task입니다.'},
         )
 
         inner_func1() >> inner_function2
 
     with TaskGroup(group_id='second_group', tooltip='두 번째 그룹입니다') as group_2:
         ''' 여기에 적은 docstring은 표시되지 않습니다'''
-        @task(task_id='inner_function1')
+        @task(task_id='inner_function1', trigger_rule='all_done')
         def inner_func1(**kwargs):
             print('두 번째 TaskGroup 내 첫 번째 task입니다.')
 
