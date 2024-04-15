af436a2a59603acb02958be099adbef185fe39c8
jonji <jon-ji@naver.com>
2024-04-15 17:38:06 +0900

    no message

--- dags/jonji_ex_dags.py
+++ dags/jonji_ex_dags.py
@@ -1,21 +1,32 @@
-from operators.jonji_ex_operator import JonjiExOperator
 from airflow import DAG
 import pendulum
-
+import datetime
+from airflow.decorators import task
+from airflow.operators.email import EmailOperator
+from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
 
 with DAG(
-    dag_id='jonji_ex_dags',
-    schedule='0 7 * * *',
-    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
-    catchup=False,
-    tags=['jonji_ex']
+    dag_id="jonji_ex_dags",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
 ) as dag:
-    '''서울시 공공자전거 대여소 정보'''
-    task_1 = JonjiExOperator(
-        task_id='task_1',
-        dataset_nm='tbCycleStationInfo',
-        path='/opt/airflow/files/tbCycleStationInfo/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='tbCycleStationInfo.csv'
+    
+    '''서울시 코로나19 확진자 발생동향'''
+    tb_corona19_count_status = SeoulApiToCsvOperator(
+        task_id='tb_corona19_count_status',
+        dataset_nm='TbCorona19CountStatus',
+        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        file_name='TbCorona19CountStatus.csv'
     )
 
-    task_1
+    send_email = EmailOperator(
+        task_id='send_email',
+        to='jon-ji@naver.com',
+        subject='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} some_logic 처리결과',
+        html_content='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} 처리 결과는 <br> \
+                    {{ti.xcom_pull(task_ids="something_task")}} 했습니다 <br>',
+        files=['/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}/TbCorona19CountStatus.csv']
+    )
+
+    tb_corona19_count_status >> send_email
