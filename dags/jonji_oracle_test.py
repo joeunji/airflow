adbdc661d252832ecddfdf9446376e6772c81dc0
jonji <jon-ji@naver.com>
2024-04-18 18:00:43 +0900

    no message

--- dags/jonji_oracle_test.py
+++ dags/jonji_oracle_test.py
@@ -6,6 +6,34 @@ from airflow import DAG
 import pendulum
 from contextlib import closing
 import pandas as pd
+import oracledb
+
+# with DAG(
+#     dag_id='jonji_oracle_test',
+#     start_date=pendulum.datetime(2023,5,1, tz='Asia/Seoul'),
+#     schedule=None,
+#     catchup=False
+# ) as dag:
+#     @task(task_id='get_emp_task')
+#     def get_emp_task():
+#         oracle_hook = OracleHook(oracle_conn_id='conn_db_oracle')
+#         with closing(oracle_hook.get_conn()) as conn:
+#             with closing(conn.cursor()) as cursor:
+#                 with open('/opt/airflow/files/sqls/jonji_test.sql', 'r') as sql_file:
+#                     sql = '\n'.join(sql_file.readlines())
+#                     cursor.execute(sql)
+#                     rslt = cursor.fetchall()
+#                     rslt = pd.DataFrame(rslt)
+#                     rslt.columns = ['EMP_NO', 'USER_ID', 'EMP_NAME']
+#                     return_blocks = []
+
+#                     return_blocks.append(sb.section_text("*사원정보*"))
+
+#                     row_data = rslt.query()
+
+#                     for idx, row in row_data.iterrows():
+#                         return_blocks.append(sb.section_text(f"{row['EMP_NO']}"))
+#                     return return_blocks
 
 with DAG(
     dag_id='jonji_oracle_test',
@@ -15,8 +43,12 @@ with DAG(
 ) as dag:
     @task(task_id='get_emp_task')
     def get_emp_task():
-        oracle_hook = OracleHook(oracle_conn_id='conn_db_oracle')
-        with closing(oracle_hook.get_conn()) as conn:
+        
+        oracledb.init_oracle_client()
+        con = oracledb.connect(user="ERPUSER", password="erpuser", dsn="118.32.191.131:1524/MISORA")
+
+        # oracle_hook = OracleHook(oracle_conn_id='conn_db_oracle')
+        with closing(con) as conn:
             with closing(conn.cursor()) as cursor:
                 with open('/opt/airflow/files/sqls/jonji_test.sql', 'r') as sql_file:
                     sql = '\n'.join(sql_file.readlines())
