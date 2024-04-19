f63396e3697b117b0f9c128f002efd269bb19550
jonji <jon-ji@naver.com>
2024-04-19 15:01:19 +0900

    no message

--- dags/jonji_oracle_test.py
+++ dags/jonji_oracle_test.py
@@ -47,8 +47,9 @@ with DAG(
         # oracledb.init_oracle_client()
         # con = oracledb.connect(user="ERPUSER", password="erpuser", dsn="118.32.191.131:1524/MISORA")
 
-        oracle_hook = OracleHook(oracle_conn_id='conn_db_oracle', thick_mode=True)
-        with closing(oracle_hook.get_conn()) as conn:
+        # oracle_hook = OracleHook(oracle_conn_id='conn_db_oracle', thick_mode=True)
+        with oracledb.connect(user='ERPUSER', password='erpuser', dsn='118.32.191.131:1524/MISORA') as conn:
+        # with closing(oracle_hook.get_conn()) as conn:
             with closing(conn.cursor()) as cursor:
                 with open('/opt/airflow/files/sqls/jonji_test.sql', 'r') as sql_file:
                     sql = '\n'.join(sql_file.readlines())
